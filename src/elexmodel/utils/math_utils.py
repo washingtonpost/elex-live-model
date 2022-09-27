5e59f8e7cd4caea67b48cb95c4fcb4626c68a44e
Dara Gold <dara.gold@washpost.com>
2022-09-26 21:16:03 -0400

    median error message

--- src/elexmodel/utils/math_utils.py
+++ src/elexmodel/utils/math_utils.py
@@ -1,6 +1,11 @@
 import numpy as np
 from scipy.stats import bootstrap
 
+class ModelClientException(Exception):
+    pass
+
+class ModelNotEnoughSubunitsException(ModelClientException):
+    pass
 
 def compute_inflate(x):
     """
@@ -30,7 +35,12 @@ def weighted_median(x, weights):
 
     # find index of largest x_i where weights are less than or equal 0.5
     weights_cumulative = np.cumsum(weights_sorted)
+    if weights_cumulative[0] > .5:
+        raise ModelNotEnoughSubunitsException(
+            "Lowest weight conformity unit is >50%. Need more data for conformity median."
+        )
     median_index = np.where(weights_cumulative <= 0.5)[0][-1]
+    
 
     # if there is one element where weights are exactly 0.5, median is average
     # otherwise weighted median is the next largest element
