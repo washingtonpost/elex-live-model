1a521730d6255fa79d579d93d8ba7da2a1f3516c
lbvienna <lenny.bronner@gmail.com>
2022-12-09 17:57:06 -0500

    fixed tests

--- tests/handlers/test_combined_data.py
+++ tests/handlers/test_combined_data.py
@@ -16,46 +16,7 @@ def test_load(va_governor_county_data):
     combined_data_handler = CombinedDataHandler(
         va_governor_county_data, current_data, estimands, "county", fixed_effects=[], handle_unreporting="drop"
     )
-
     assert combined_data_handler.data.shape == (133, 29)
-
-
-def test_generate_fixed_effects(va_governor_county_data):
-    estimands = ["turnout"]
-    live_data_handler = MockLiveDataHandler(
-        "2017-11-07_VA_G", "G", "county", estimands=["turnout"], data=va_governor_county_data
-    )
-    current_data = live_data_handler.data
-
-    combined_data_handler = CombinedDataHandler(
-        va_governor_county_data,
-        current_data,
-        estimands,
-        "county",
-        fixed_effects=["county_classification"],
-        handle_unreporting="drop",
-    )
-
-    assert combined_data_handler.data.shape == (133, 34)  # 29 + 6 - 1 (subtracting dropped column)
-    assert "county_classification_nova" in combined_data_handler.data.columns
-    assert "county_classification" in combined_data_handler.fixed_effects
-    assert len(combined_data_handler.expanded_fixed_effects) == 5  # 6 - 1
-
-    combined_data_handler = CombinedDataHandler(
-        va_governor_county_data,
-        current_data,
-        estimands,
-        "county",
-        fixed_effects=["county_classification", "county_fips"],
-        handle_unreporting="drop",
-    )
-
-    assert combined_data_handler.data.shape == (133, 166)  # 29 + 6 + 133 - 2 (subtracting two dropped columns)= 166
-    assert "county_classification_nova" in combined_data_handler.data.columns
-    assert "county_fips_51790" in combined_data_handler.data.columns
-    assert "county_classification" in combined_data_handler.fixed_effects
-    assert "county_fips" in combined_data_handler.fixed_effects
-    assert len(combined_data_handler.expanded_fixed_effects) == 137  # 6 + 133 - 2
 
 
 def test_zero_unreporting_missing_single_estimand_value(va_governor_county_data):
@@ -158,9 +119,79 @@ def test_drop_unreporting_missing_single_estimand_value(va_governor_county_data)
     assert combined_data_handler.data["results_dem"].iloc[0] != 0  # didn't accidentally set other to zero
 
 
+def test_generate_fixed_effects(va_governor_county_data):
+    election_id = "2017-11-07_VA_G"
+    office = "G"
+    geographic_unit_type = "county"
+    estimands = ["turnout"]
+    estimand_baseline = {"turnout": "turnout"}
+    live_data_handler = MockLiveDataHandler(
+        election_id, office, geographic_unit_type, estimands, data=va_governor_county_data
+    )
+    current_data = live_data_handler.get_n_fully_reported(n=va_governor_county_data.shape[0])
+
+    preprocessed_data_handler = PreprocessedDataHandler(
+        election_id, office, geographic_unit_type, estimands, estimand_baseline, data=va_governor_county_data
+    )
+
+    combined_data_handler = CombinedDataHandler(
+        preprocessed_data_handler.data,
+        current_data,
+        estimands,
+        "county",
+        fixed_effects=["county_classification"],
+        handle_unreporting="drop",
+    )
+    
+    reporting_data = combined_data_handler.get_reporting_units(99)
+    nonreporting_data = combined_data_handler.get_nonreporting_units(99)
+
+    assert combined_data_handler.data.shape == (133, 33)
+
+    n_expected_columns = combined_data_handler.data.shape[1] + 3 # residual intercept and reporting
+    n_expected_columns += 5 # 6 - 1 the fixed effects with one dropped
+    assert reporting_data.shape == (133, n_expected_columns)
+    assert nonreporting_data.shape == (0, n_expected_columns)
+
+    assert "county_classification_nova" in reporting_data.columns
+    assert "county_classification_nova" in nonreporting_data.columns
+
+    assert "county_classification" in combined_data_handler.fixed_effects
+    assert len(combined_data_handler.expanded_fixed_effects) == 5  # 6 - 1
+
+    combined_data_handler = CombinedDataHandler(
+        va_governor_county_data,
+        current_data,
+        estimands,
+        "county",
+        fixed_effects=["county_classification", "county_fips"],
+        handle_unreporting="drop",
+    )
+
+    reporting_data = combined_data_handler.get_reporting_units(99)
+    nonreporting_data = combined_data_handler.get_nonreporting_units(99)
+
+    assert combined_data_handler.data.shape == (133, 33)
+
+    n_expected_columns = combined_data_handler.data.shape[1] + 3 # residual intercept and reporting
+    n_expected_columns += 6 + 133 - 2 # subtracting two dropped columns
+    assert reporting_data.shape == (133, n_expected_columns) 
+    assert nonreporting_data.shape == (0, n_expected_columns) 
+
+    assert "county_classification_nova" in reporting_data.columns
+    assert "county_classification_nova" in nonreporting_data.columns
+
+    assert "county_fips_51790" in reporting_data.columns
+    assert "county_fips_51790" in nonreporting_data.columns
+
+    assert "county_classification" in combined_data_handler.fixed_effects
+    assert "county_fips" in combined_data_handler.fixed_effects
+    assert len(combined_data_handler.expanded_fixed_effects) == 137  # 6 + 133 - 2
+
+
 def test_expanding_fixed_effects_basic():
     df = pd.DataFrame({"c1": ["a", "b", "b", "c"], "c2": ["w", "x", "y", "z"], "c3": [2, 4, 1, 9]})
-    expanded = CombinedDataHandler._expand_fixed_effects(df, ["c1"])
+    expanded = CombinedDataHandler._expand_fixed_effects(df, ["c1"], drop_first=True)
     pd.testing.assert_frame_equal(
         expanded,
         pd.DataFrame(
@@ -174,7 +205,7 @@ def test_expanding_fixed_effects_basic():
         ),
     )
 
-    expanded = CombinedDataHandler._expand_fixed_effects(df, ["c1", "c2"])
+    expanded = CombinedDataHandler._expand_fixed_effects(df, ["c1", "c2"], drop_first=True)
     pd.testing.assert_frame_equal(
         expanded,
         pd.DataFrame(
