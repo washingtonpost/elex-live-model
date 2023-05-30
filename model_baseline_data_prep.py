ca458376246a28749597bc0bcfadf856c69741b5
Dara Gold <dara.gold@washpost.com>
2023-05-30 18:24:23 -0400

    remove orig data

--- model_baseline_data_prep.py
+++ model_baseline_data_prep.py
@@ -1,18 +1,6 @@
 import pandas as pd
 
-# read in 2022 preprocessed data for county-senate model, whose baseline columns we will replace
-# columns are baseline_dem and baseline_rep which for now are 2020 presidential returns
-old_baseline_data_path = (
-    "../../elex-live-model-data-processing/2022-11-08_USA_G/final_processed_data/S_county/data_county.csv"
-)
-old_data = pd.read_csv(old_baseline_data_path, dtype={"geographic_unit_fips": str}).rename(
-    columns={
-        "baseline_dem": "baseline_dem_old",
-        "baseline_gop": "baseline_gop_old",
-        "baseline_turnout": "baseline_turnout_old",
-    }
-)
-
+# Model new baseline for 2022 senate model
 # pull in county-level data for blending
 # we are interested in county-town level data for statewide elections
 # over multiple years
@@ -65,6 +53,4 @@ all_results = all_results.rename(
 
 # replace new baseline into 2022 preprocessed data
 new_data = all_results[["geographic_unit_fips", "baseline_dem", "baseline_gop", "baseline_turnout"]]
-
-new_data_full = pd.merge(old_data, new_data, on="geographic_unit_fips")
 new_data.to_csv("new_baseline_data_S_county_2022.csv", index=False)
