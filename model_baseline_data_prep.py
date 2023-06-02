b1805e64cc33697d0d0a2ca9f6ccaa10a6e24ae3
Dara Gold <dara.gold@washpost.com>
2023-06-02 17:06:52 -0400

    formatted

--- model_baseline_data_prep.py
+++ model_baseline_data_prep.py
@@ -43,7 +43,16 @@ all_results = all_results.drop(["baseline_dem", "baseline_gop", "baseline_turnou
 # CHECK HERE TURNOUT THING
 all_results["results_dem_share"] = all_results["results_dem"] / (all_results["results_turnout"])
 all_results["results_gop_share"] = all_results["results_gop"] / (all_results["results_turnout"])
-all_results["results_turnout_share"] = all_results["results_turnout"] / (all_results["total_people"])
+
+# For turnout we don't want to have both P and S/G in the blend, because
+# turnout varies so greatly, and we need to use a CONSTANT denominator (total people, or registered voters etc.)
+# for a turnout share. For the Senate 2022 example, drop P from blend
+list_of_dfs_turnout = [sen_16, gov_18, gov_16]
+turnout_df = pd.concat(list_of_dfs_turnout).reset_index(drop=True)
+turnout_df["results_turnout_share"] = turnout_df["results_turnout"] / turnout_df["total_people"]
+turnout_cols = turnout_df[["geographic_unit_fips", "results_turnout_share"]]
+
+all_results = pd.merge(all_results, turnout_cols, on="geographic_unit_fips")
 all_results = all_results.groupby(["geographic_unit_fips"]).mean().reset_index(drop=False)
 all_results = all_results.rename(
     columns={
