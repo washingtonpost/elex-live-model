68719efe10c0263dec3ea35b2ddaddc741252545
rishasurana <risha.surana@washingtonpost.com>
2023-06-29 14:39:41 -0400

    set-up for linter

--- src/elexmodel/handlers/data/Estimandizer.py
+++ src/elexmodel/handlers/data/Estimandizer.py
@@ -1,15 +1,11 @@
-import numpy as np
-import pandas as pd
-
-
 class Estimandizer:
     """
     Generate estimands expicitly.
     """
+
     def __init__(self, data_handler, estimands):
         self.data_handler = data_handler
         self.estimands = estimands
-    
 
     def check_estimand(estimand):
         already_included = ["dem_votes", "gop_votes", "total_votes"]
--- tests/handlers/test_estimandizer.py
+++ tests/handlers/test_estimandizer.py
@@ -1,3 +1,4 @@
+"""
 import numpy as np
 import pandas as pd
 
@@ -5,4 +6,4 @@ from elexmodel.handlers.data.CombinedData import CombinedDataHandler
 from elexmodel.handlers.data.Estimandizer import Estimandizer
 from elexmodel.handlers.data.LiveData import MockLiveDataHandler
 from elexmodel.handlers.data.PreprocessedData import PreprocessedDataHandler
-
+"""
