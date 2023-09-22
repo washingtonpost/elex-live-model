440a7e06b1cabd0fe2607d78f203dc232808762a
lbvienna <lenny.bronner@gmail.com>
2023-09-22 10:49:31 -0400

    updated estimandizer unit tests

--- tests/handlers/test_estimandizer.py
+++ tests/handlers/test_estimandizer.py
@@ -1,3 +1,5 @@
+import pytest
+
 from elexmodel.handlers.data.Estimandizer import Estimandizer
 
 
@@ -13,6 +15,7 @@ def test_add_estimand_results_not_historical(va_governor_county_data):
     (output_df, result_columns) = estimandizer.add_estimand_results(va_data_copy, estimands, False)
 
     assert "results_party_vote_share_dem" in output_df.columns
+    assert "results_weights" in output_df.columns
     assert result_columns == ["results_party_vote_share_dem", "results_turnout"]
 
 
@@ -27,6 +30,7 @@ def test_add_estimand_results_historical(va_governor_county_data):
     (output_df, result_columns) = estimandizer.add_estimand_results(va_data_copy, estimands, True)
 
     assert "results_party_vote_share_dem" in output_df.columns
+    assert "results_weights" in output_df.columns
     assert result_columns == ["results_party_vote_share_dem", "results_turnout"]
 
 
@@ -34,6 +38,7 @@ def test_add_estimand_baselines_not_historical(va_governor_county_data):
     estimand_baselines = {"turnout": "turnout", "party_vote_share_dem": "party_vote_share_dem"}
     estimandizer = Estimandizer()
     output_df = estimandizer.add_estimand_baselines(va_governor_county_data.copy(), estimand_baselines, False)
+    assert "baseline_weights" in output_df.columns
     assert "baseline_party_vote_share_dem" in output_df.columns
     assert "last_election_results_party_vote_share_dem" in output_df.columns
 
@@ -45,5 +50,26 @@ def test_add_estimand_baselines_historical(va_governor_county_data):
         va_governor_county_data.copy(), estimand_baselines, True, include_results_estimand=True
     )
     assert "baseline_party_vote_share_dem" in output_df.columns
+    assert "baseline_weights" in output_df.columns
     assert "results_party_vote_share_dem" in output_df.columns
     assert "last_election_results_party_vote_share_dem" not in output_df.columns
+
+
+def test_add_turnout_factor(va_governor_county_data):
+    estimands = ["party_vote_share_dem", "turnout"]
+    estimand_baselines = {"turnout": "turnout", "party_vote_share_dem": "party_vote_share_dem"}
+    estimandizer = Estimandizer()
+    output_df = estimandizer.add_estimand_baselines(
+        va_governor_county_data.copy(), estimand_baselines, False, include_results_estimand=False
+    )
+    output_df, __ = estimandizer.add_estimand_results(output_df, estimands, False)
+
+    # check that nan turns into 0
+    output_df.loc[0, "baseline_weights"] = 0.0
+    import pdb
+
+    pdb.set_trace()
+    output_df = estimandizer.add_turnout_factor(output_df)
+
+    assert "turnout_factor" in output_df.columns
+    assert 0 == pytest.approx(output_df.loc[0, "turnout_factor"])
