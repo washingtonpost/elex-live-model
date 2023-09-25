88719c7e01822d98db30d5f42d140fb6b24f9d3f
lbvienna <lenny.bronner@gmail.com>
2023-09-25 15:04:59 -0400

    changed tests slightly

--- tests/handlers/test_combined_data.py
+++ tests/handlers/test_combined_data.py
@@ -16,21 +16,13 @@ def test_load(va_governor_county_data):
         election_id, office_id, geographic_unit_type, estimands=["turnout"], data=va_governor_county_data
     )
     current_data = live_data_handler.data
-    preprocessed_data_handler = PreprocessedDataHandler(
-        election_id,
-        office_id,
-        geographic_unit_type,
-        estimand_baselines=estimand_baselines,
-        estimands=estimands,
-        data=va_governor_county_data,
-    )
-    preprocessed_data = preprocessed_data_handler.data
+    va_governor_county_data['baseline_weights'] = va_governor_county_data.baseline_turnout
 
     combined_data_handler = CombinedDataHandler(
-        preprocessed_data, current_data, estimands, "county", handle_unreporting="drop"
+        va_governor_county_data, current_data, estimands, "county", handle_unreporting="drop"
     )
 
-    assert combined_data_handler.data.shape == (133, 33)
+    assert combined_data_handler.data.shape == (133, 32)
 
 
 def test_zero_unreporting_missing_single_estimand_value(va_governor_county_data):
@@ -49,25 +41,17 @@ def test_zero_unreporting_missing_single_estimand_value(va_governor_county_data)
     current_data["percent_expected_vote"] = 100
     current_data.loc[0, "results_dem"] = np.nan
 
-    preprocessed_data_handler = PreprocessedDataHandler(
-        election_id,
-        office_id,
-        geographic_unit_type,
-        estimand_baselines=estimand_baselines,
-        estimands=estimands,
-        data=va_governor_county_data,
-    )
-    preprocessed_data = preprocessed_data_handler.data
+    va_governor_county_data['baseline_weights'] = va_governor_county_data.baseline_turnout
 
     combined_data_handler = CombinedDataHandler(
-        preprocessed_data, current_data, estimands, "county", handle_unreporting="zero"
+        va_governor_county_data, current_data, estimands, "county", handle_unreporting="zero"
     )
     assert combined_data_handler.data["results_dem"].iloc[0] == 0.0  # value with na result has been set to zero
     assert combined_data_handler.data["results_turnout"].iloc[0] != 0  # has not been set to zero
     assert (
         combined_data_handler.data["percent_expected_vote"].iloc[0] == 0
     )  # percent expected vote with na result has been set to zero
-    assert combined_data_handler.data.shape == (133, 36)  # didn't drop any
+    assert combined_data_handler.data.shape == (133, 34)  # didn't drop any
     assert combined_data_handler.data["results_dem"].iloc[1] != 0  # didn't accidentally set other to zero
 
 
@@ -88,23 +72,15 @@ def test_zero_unreporting_missing_multiple_estimands_value(va_governor_county_da
     current_data.loc[0, "results_dem"] = np.nan
     current_data.loc[0, "results_turnout"] = np.nan
 
-    preprocessed_data_handler = PreprocessedDataHandler(
-        election_id,
-        office_id,
-        geographic_unit_type,
-        estimand_baselines=estimand_baselines,
-        estimands=estimands,
-        data=va_governor_county_data,
-    )
-    preprocessed_data = preprocessed_data_handler.data
+    va_governor_county_data['baseline_weights'] = va_governor_county_data.baseline_turnout
 
     combined_data_handler = CombinedDataHandler(
-        preprocessed_data, current_data, estimands, "county", handle_unreporting="zero"
+        va_governor_county_data, current_data, estimands, "county", handle_unreporting="zero"
     )
     assert combined_data_handler.data["results_dem"].iloc[0] == 0.0
     assert combined_data_handler.data["results_turnout"].iloc[0] == 0.0
     assert combined_data_handler.data["percent_expected_vote"].iloc[0] == 0.0
-    assert combined_data_handler.data.shape == (133, 36)
+    assert combined_data_handler.data.shape == (133, 34)
     assert combined_data_handler.data["results_dem"].iloc[1] != 0  # didn't accidentally set other to zero
     assert combined_data_handler.data["results_turnout"].iloc[1] != 0  # didn't accidentally set other to zero
 
@@ -126,22 +102,14 @@ def test_zero_unreporting_missing_percent_expected_vote_value(va_governor_county
     current_data.loc[0, "percent_expected_vote"] = np.nan
     current_data.loc[0, "results_dem"] = np.nan
 
-    preprocessed_data_handler = PreprocessedDataHandler(
-        election_id,
-        office_id,
-        geographic_unit_type,
-        estimand_baselines=estimand_baselines,
-        estimands=estimands,
-        data=va_governor_county_data,
-    )
-    preprocessed_data = preprocessed_data_handler.data
+    va_governor_county_data['baseline_weights'] = va_governor_county_data.baseline_turnout
 
     combined_data_handler = CombinedDataHandler(
-        preprocessed_data, current_data, estimands, "county", handle_unreporting="zero"
+        va_governor_county_data, current_data, estimands, "county", handle_unreporting="zero"
     )
     assert combined_data_handler.data["results_dem"].iloc[0] == 0.0
     assert combined_data_handler.data["percent_expected_vote"].iloc[0] == 0.0
-    assert combined_data_handler.data.shape == (133, 36)
+    assert combined_data_handler.data.shape == (133, 34)
     assert combined_data_handler.data["results_dem"].iloc[1] != 0  # didn't accidentally set other to zero
 
 
@@ -161,22 +129,14 @@ def test_zero_unreporting_random_percent_expected_vote_value(va_governor_county_
     current_data["percent_expected_vote"] = np.random.randint(1, 100, current_data.shape[0])
     current_data.loc[0, "results_dem"] = np.nan
 
-    preprocessed_data_handler = PreprocessedDataHandler(
-        election_id,
-        office_id,
-        geographic_unit_type,
-        estimand_baselines=estimand_baselines,
-        estimands=estimands,
-        data=va_governor_county_data,
-    )
-    preprocessed_data = preprocessed_data_handler.data
+    va_governor_county_data['baseline_weights'] = va_governor_county_data.baseline_turnout
 
     combined_data_handler = CombinedDataHandler(
-        preprocessed_data, current_data, estimands, "county", handle_unreporting="zero"
+        va_governor_county_data, current_data, estimands, "county", handle_unreporting="zero"
     )
     assert combined_data_handler.data["results_dem"].iloc[0] == 0.0  # all values set to 0.0
     assert combined_data_handler.data["percent_expected_vote"].iloc[0] == 0.0
-    assert combined_data_handler.data.shape == (133, 36)
+    assert combined_data_handler.data.shape == (133, 34)
     assert combined_data_handler.data["results_dem"].iloc[1] != 0  # didn't accidentally set other to zero
 
 
@@ -196,20 +156,12 @@ def test_drop_unreporting_missing_single_estimand_value(va_governor_county_data)
     current_data["percent_expected_vote"] = 100
     current_data.loc[0, "results_dem"] = np.nan
 
-    preprocessed_data_handler = PreprocessedDataHandler(
-        election_id,
-        office_id,
-        geographic_unit_type,
-        estimand_baselines=estimand_baselines,
-        estimands=estimands,
-        data=va_governor_county_data,
-    )
-    preprocessed_data = preprocessed_data_handler.data
+    va_governor_county_data['baseline_weights'] = va_governor_county_data.baseline_turnout
 
     combined_data_handler = CombinedDataHandler(
-        preprocessed_data, current_data, estimands, "county", handle_unreporting="drop"
+        va_governor_county_data, current_data, estimands, "county", handle_unreporting="drop"
     )
-    assert combined_data_handler.data.shape == (132, 36)  # dropped one
+    assert combined_data_handler.data.shape == (132, 34)  # dropped one
     assert combined_data_handler.data["results_dem"].iloc[0] != 0  # didn't accidentally set other to zero
 
 
@@ -224,13 +176,13 @@ def test_get_reporting_data(va_governor_county_data):
         election_id, office, geographic_unit_type, estimands, data=va_governor_county_data
     )
     current_data = live_data_handler.get_n_fully_reported(n=20)
-    preprocessed_data_handler = PreprocessedDataHandler(
-        election_id, office, geographic_unit_type, estimands, estimand_baseline, data=va_governor_county_data
-    )
+
+    va_governor_county_data['baseline_weights'] = va_governor_county_data.baseline_turnout
+    va_governor_county_data['last_election_results_turnout'] = va_governor_county_data.baseline_turnout + 1
 
     # no fixed effects
     combined_data_handler = CombinedDataHandler(
-        preprocessed_data_handler.data, current_data, estimands, geographic_unit_type
+        va_governor_county_data, current_data, estimands, geographic_unit_type
     )
     observed_data = combined_data_handler.get_reporting_units(100)
     assert observed_data.shape[0] == 20
@@ -249,12 +201,12 @@ def test_get_reporting_data_dropping_with_turnout_factor(va_governor_county_data
         election_id, office, geographic_unit_type, estimands, data=va_governor_county_data
     )
     current_data = live_data_handler.get_n_fully_reported(n=20)
-    preprocessed_data_handler = PreprocessedDataHandler(
-        election_id, office, geographic_unit_type, estimands, estimand_baseline, data=va_governor_county_data
-    )
+    
+    va_governor_county_data['baseline_weights'] = va_governor_county_data.baseline_turnout
+    va_governor_county_data['last_election_results_turnout'] = va_governor_county_data.baseline_turnout + 1
 
     combined_data_handler = CombinedDataHandler(
-        preprocessed_data_handler.data, current_data, estimands, geographic_unit_type
+        va_governor_county_data, current_data, estimands, geographic_unit_type
     )
 
     turnout_factor_lower = 0.95
@@ -290,12 +242,12 @@ def test_get_nonreporting_adding_with_turnout_factor(va_governor_county_data):
     )
     n = 20
     current_data = live_data_handler.get_n_fully_reported(n=n)
-    preprocessed_data_handler = PreprocessedDataHandler(
-        election_id, office, geographic_unit_type, estimands, estimand_baseline, data=va_governor_county_data
-    )
+    
+    va_governor_county_data['baseline_weights'] = va_governor_county_data.baseline_turnout
+    va_governor_county_data['last_election_results_turnout'] = va_governor_county_data.baseline_turnout + 1
 
     combined_data_handler = CombinedDataHandler(
-        preprocessed_data_handler.data, current_data, estimands, geographic_unit_type
+        va_governor_county_data, current_data, estimands, geographic_unit_type
     )
 
     turnout_factor_lower = 0.95
@@ -339,12 +291,12 @@ def test_get_unexpected_units_county_district(va_assembly_county_data):
         unexpected_units=unexpected_units,
     )
     current_data = live_data_handler.get_n_fully_reported(n=20)
-    preprocessed_data_handler = PreprocessedDataHandler(
-        election_id, office, geographic_unit_type, estimands, estimand_baseline, data=va_assembly_county_data
-    )
+    
+    va_assembly_county_data['baseline_weights'] = va_assembly_county_data.baseline_turnout
+    va_assembly_county_data['last_election_results_turnout'] = va_assembly_county_data.baseline_turnout + 1
 
     combined_data_handler = CombinedDataHandler(
-        preprocessed_data_handler.data, current_data, estimands, geographic_unit_type
+        va_assembly_county_data, current_data, estimands, geographic_unit_type
     )
     unexpected_data = combined_data_handler.get_unexpected_units(100, ["county_fips", "district"])
     assert unexpected_data.shape[0] == unexpected_units
@@ -377,12 +329,11 @@ def test_get_unexpected_units_county(va_governor_county_data):
     extra_row["percent_expected_vote"] = 50
     current_data = pd.concat([current_data, extra_row])
 
-    preprocessed_data_handler = PreprocessedDataHandler(
-        election_id, office, geographic_unit_type, estimands, estimand_baseline, data=va_governor_county_data
-    )
+    va_governor_county_data['baseline_weights'] = va_governor_county_data.baseline_turnout
+    va_governor_county_data['last_election_results_turnout'] = va_governor_county_data.baseline_turnout + 1
 
     combined_data_handler = CombinedDataHandler(
-        preprocessed_data_handler.data, current_data, estimands, geographic_unit_type
+        va_governor_county_data, current_data, estimands, geographic_unit_type
     )
     unexpected_data = combined_data_handler.get_unexpected_units(100, ["county_fips"])
     assert unexpected_data.shape[0] == reporting_unexpected_units + 1
