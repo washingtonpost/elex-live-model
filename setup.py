7128c546a896600139ee42e616f9d54b1428be4d
Diane Napolitano <diane.napolitano@washpost.com>
2023-10-19 09:43:38 -0400

    Increasing required version numbers in setup.py with tilde versions

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from codecs import open
 
 from setuptools import find_packages, setup
 
-INSTALL_REQUIRES = ("click<8.1", "elex-solver<3", "pandas<1.5.0", "boto3<2", "python-dotenv==0.19.2", "scipy==1.10.1")
+INSTALL_REQUIRES = ("click~=8.1", "elex-solver", "pandas~=2.1", "boto3~=1.28", "python-dotenv~=1.0", "scipy~=1.11")
 
 THIS_FILE_DIR = os.path.dirname(__file__)
 
