e695b1d93cb2559d40d9e0cf7aacd2782bdd671e
Diane Napolitano <diane.napolitano@washpost.com>
2024-09-12 15:51:46 -0400

    Moving the adding of pred_turnout to the ModelResults to a separate method; starting work on a unit test:

--- tests/handlers/test_model_results.py
+++ tests/handlers/test_model_results.py
@@ -58,13 +58,21 @@ intervals2_e1 = {0.7: PredictionIntervals([3900], [4600]), 0.9: PredictionInterv
 intervals1_e2 = {0.7: PredictionIntervals([400, 1600], [400, 1800]), 0.9: PredictionIntervals([400, 1500], [400, 2000])}
 intervals2_e2 = {0.7: PredictionIntervals([2000], [2200]), 0.9: PredictionIntervals([1800], [2400])}
 
+reporting_with_turnout = reporting.copy().drop(columns=["results_e2"])
+reporting_with_turnout["results_weights"] = [100000, 200000]
+nonreporting_turnout = [0]
+notexpected_with_turnout = notexpected.copy().drop(columns=["results_e2"])
+notexpected_with_turnout["results_weights"] = [0]
+
 
 def test_model_results_handler():
     # test unit predictions/intervals methods for two estimands
-    handler = ModelResultsHandler(["district", "unit", "postal_code"], [0.7, 0.9], reporting, nonreporting, notexpected)
-    handler.add_unit_predictions("e1", predictions_e1, None)
+    handler = ModelResultsHandler(
+        ["district", "unit", "postal_code"], [0.7, 0.9], reporting.copy(), nonreporting.copy(), notexpected.copy()
+    )
+    handler.add_unit_predictions("e1", predictions_e1)
     handler.add_unit_intervals("e1", intervals_e1)
-    handler.add_unit_predictions("e2", predictions_e2, None)
+    handler.add_unit_predictions("e2", predictions_e2)
     handler.add_unit_intervals("e2", intervals_e2)
     expected_cols = [
         "pred_e1",
@@ -105,11 +113,59 @@ def test_model_results_handler():
 
 
 def test_no_unit_data():
-    handler = ModelResultsHandler(["postal_code"], [0.7, 0.9], reporting, nonreporting, notexpected)
-    handler.add_unit_predictions("e1", predictions_e1, None)
+    handler = ModelResultsHandler(
+        ["postal_code"], [0.7, 0.9], reporting.copy(), nonreporting.copy(), notexpected.copy()
+    )
+    handler.add_unit_predictions("e1", predictions_e1)
     handler.add_unit_intervals("e1", intervals_e1)
 
     handler.add_agg_predictions("e1", "postal_code", agg1_e1, intervals1_e1)
     handler.process_final_results()
 
     assert "unit_data" not in handler.final_results
+
+
+def test_add_turnout_results():
+    handler = ModelResultsHandler(
+        ["district", "unit", "postal_code"],
+        [0.7, 0.9],
+        reporting_with_turnout.copy(),
+        nonreporting.copy(),
+        notexpected_with_turnout.copy(),
+    )
+    handler.add_unit_predictions("e1", predictions_e1)
+    handler.add_unit_intervals("e1", intervals_e1)
+    handler.add_unit_turnout_predictions(nonreporting_turnout)
+    expected_cols = [
+        "pred_e1",
+        "lower_0.7_e1",
+        "lower_0.9_e1",
+        "upper_0.7_e1",
+        "upper_0.9_e1",
+        "results_weights",
+        "pred_turnout",
+    ]
+
+    assert set(expected_cols).issubset(set(handler.reporting_units))
+    assert set(expected_cols).difference({"results_weights"}).issubset(set(handler.nonreporting_units))
+    assert set(expected_cols).issubset(set(handler.unexpected_units))
+
+    # # test agg predictions for 2 aggs and 2 estimands
+    # handler.add_agg_predictions("e1", "district", agg1_e1, intervals1_e1)
+    # handler.add_agg_predictions("e1", "postal_code", agg2_e1, intervals2_e1)
+
+    # assert len(handler.estimates["postal_code"]) == 2
+    # assert len(handler.estimates["district"]) == 2
+    # expected_cols_agg = ["pred", "lower_0.7", "lower_0.9", "upper_0.7", "upper_0.9"]
+    # for v in handler.estimates.values():
+    #     for i, df in enumerate(v):
+    #         expected_cols = [f"{x}_e{i+1}" for x in expected_cols_agg]
+    #         assert set(expected_cols).issubset(set(df.columns))
+
+    # # test preparation of final results data
+    # handler.process_final_results()
+
+    # assert set(handler.final_results.keys()) == set(["unit_data", "state_data", "district_data"])
+    # assert len(handler.final_results["unit_data"]) == 4
+    # assert len(handler.final_results["state_data"]) == 1
+    # assert len(handler.final_results["district_data"]) == 2
