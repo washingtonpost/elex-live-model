eed1107718f726995bf5ce7caea37d277a06d4ce
Diane Napolitano <diane.napolitano@washpost.com>
2024-09-18 11:58:16 -0400

    Multiplying the std by 5 seems better / promising

--- src/elexmodel/handlers/data/CombinedData.py
+++ src/elexmodel/handlers/data/CombinedData.py
@@ -140,8 +140,8 @@ class CombinedDataHandler:
         )
         tf_by_state.columns = tf_by_state.columns.map(lambda x: "_".join(x))
         tf_by_state = tf_by_state.reset_index()
-        tf_by_state["tf_lower"] = tf_by_state["turnout_factor_mean"] - (tf_by_state["turnout_factor_std"] * 3)
-        tf_by_state["tf_upper"] = tf_by_state["turnout_factor_mean"] + (tf_by_state["turnout_factor_std"] * 3)
+        tf_by_state["tf_lower"] = tf_by_state["turnout_factor_mean"] - (tf_by_state["turnout_factor_std"] * 5)
+        tf_by_state["tf_upper"] = tf_by_state["turnout_factor_mean"] + (tf_by_state["turnout_factor_std"] * 5)
 
         units_with_strange_turnout_factor = units_with_strange_turnout_factor.merge(
             tf_by_state, on="postal_code", how="left"
