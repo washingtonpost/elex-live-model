64ea03d95c2313b3c3e1a33a5b9a6a99905c352e
Diane Napolitano <diane.napolitano@washpost.com>
2024-11-05 21:40:32 -0500

    Preparing for 2.2.4

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open(os.path.join(THIS_FILE_DIR, "README.md"), encoding="utf-8") as f:
     LONG_DESCRIPTION = f.read()
 
 # The full version, including alpha/beta/rc tags
-RELEASE = "2.2.3"
+RELEASE = "2.2.4"
 # The short X.Y version
 VERSION = ".".join(RELEASE.split(".")[:2])
 
